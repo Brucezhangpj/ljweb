dc0dcc08e7f38885e37b3e3e52939f3ef0bb2561
pingjun.zhang <pingjun.zhang@geely.com>
2022-05-18 14:46:20 +0800

    项目初始化

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>网站升级中...</h1>
+  </div>
+</template>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    devServer: {
+        port: 8888,   // 端口号
+    },
+
+    assetsDir: 'static',
+    parallel: false,
+    publicPath: './',
+
+};
